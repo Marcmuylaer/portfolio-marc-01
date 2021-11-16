5e71e7e21e7c5399adc82b438bf31c1fb19d27be
Marcmuylaer <88653486+Marcmuylaer@users.noreply.github.com>
2021-11-16 16:49:57 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import                    "bootstrap/dist/css/bootstrap.min.css";
-import                    "./App.css";
-import React         from 'react';
-import Particles     from "react-particles-js";
-import Navbar        from "./Components/Navbar";
-import Header        from "./Components/Header";
-import AboutMe       from "./Components/AboutMe";
-import Portfolio     from './Components/Portfolio';
-import Contact       from './Components/Contact';
-import Footer        from './Components/Footer';
-//import FakeNav     from "./Components/FakeNav";
-//import Skills      from "./Components/Skills";
-import Timeline      from './Components/Experiences';
+import "./App.css";
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Navbar from "./Components/Navbar";
+import FakeNav from "./Components/FakeNav";
+import Header from "./Components/Header";
+import AboutMe from "./Components/AboutMe"
+import Skills from "./Components/Skills"
+import Timeline from './Components/Experience/Timeline'
+import Portfolio from './Components/Portfolio/Portfolio-Con'
+import ContactForm from './Components/Contact'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div>
       <Particles
-        className="particles-canvas"
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900,
-              },
-            },
-            shape: {
-              type: "spiral",
-              stroke: {
-                with: 6,
-                color: "#f9ab00",
-              },
-            },
+      className="particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
           },
-        }}
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 6,
+              color: "#156307"
+            }
+          }
+        }
+      }}
       />
+      <FakeNav />
       <Navbar />
       <Header />
       <AboutMe />
       <Skills />
-      <Experiences />
+      <Timeline />
       <Portfolio />
-      <Contact />
+      <ContactForm />
       <Footer />
     </div>
   );
---
+++ src/Components/Header/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Typed from 'react-typed'
+import { Link } from 'react-scroll'
+
+const Header = () => {
+
+    return (
+        <div className="header-wraper" id="Home">
+            <div className="main-info">
+                <h1>Developer</h1>
+                <Typed
+                className="typed-text"
+                strings={["Web Developement", "Mainframe Development", "Technical analyst"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+                />
+             
+                <Link
+                className="btn-main-offer"
+                activeClass="active"
+                to="Contact"
+                spy={true}
+                smooth={true}
+                hashSpy={true}
+                offset={50}
+                duration={500}
+              >
+                {" "}
+                Contact Me
+              </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
